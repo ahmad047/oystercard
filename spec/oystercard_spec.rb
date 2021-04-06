4387fda62379497bc0c77c755122efc5d10d77eb
Muhammad Nasr Ahmad <79932064+ahmad047@users.noreply.github.com>
2021-04-06 16:48:40 +0100

    #5 enabled top_up functionality

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,4 +7,15 @@ describe OysterCard do
       expect(subject.balance).to eq(0)
     end
   end
+
+  describe '#top_up' do
+
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it 'tops up the balance' do
+      card = subject
+      card.top_up(50)
+      expect(card.balance).to eq(50)
+    end
+  end
 end
