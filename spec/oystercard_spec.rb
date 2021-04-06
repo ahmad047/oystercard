b9bb6023a4832af0243d124946afcbeb1aa7ecf8
Adam <adam.j.mccor@gmail.com>
2021-04-06 16:11:16 +0100

    #3 created OysterCard balance starting at 0

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,10 @@
 require 'oystercard'
 
 describe OysterCard do
-  
+
+  describe '#balance' do
+    it 'creates new card with 0 balance' do
+      expect(subject.balance).to eq(0)
+    end
+  end
 end
