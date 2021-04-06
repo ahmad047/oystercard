922617e9ad605fa76c5e220c632716baf6365693
Adam <adam.j.mccor@gmail.com>
2021-04-06 18:37:27 +0100

    #6 Enforce maximum balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,18 @@
 class OysterCard
+  MAXIMUM_LIMIT = 90
 
-  attr_reader :balance
+  attr_reader :balance, :MAXIMUM_LIMIT
 
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
+    fail "Max limit of #{MAXIMUM_LIMIT}, cannot top-up" if amount + balance > MAXIMUM_LIMIT
     @balance += amount
   end
+
+  # def max_limit?
+  #   @balance > MAXIMUM_LIMIT
+  # end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -17,5 +17,24 @@ describe OysterCard do
       card.top_up(50)
       expect(card.balance).to eq(50)
     end
+
+    it 'raises an error when max_limit reached' do
+      maximum_limit = OysterCard::MAXIMUM_LIMIT
+      subject.top_up maximum_limit
+      expect { subject.top_up 1 }.to raise_error "Max limit of #{maximum_limit}, cannot top-up"
+    end
   end
+
+  # describe '#max_limit?' do
+
+  #   it 'returns false when max_limit not reached' do
+  #     expect(subject.max_limit?).to be(false)
+  #   end
+
+    # it 'returns true when max_limit reached' do
+    #   card = subject
+    #   card.top_up(90)
+    #   expect(card.max_limit?).to be(true)
+    # end
+  # end
 end
